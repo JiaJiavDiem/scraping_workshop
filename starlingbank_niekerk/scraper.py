0a39a139b7b14adf0d63ee19b5bbaad5f8de923d
Hannes Datta <h.datta@tilburguniversity.edu>
2019-10-10 12:49:47 +0200

    incorporate step2AB in overall scraper

--- starlingbank_niekerk/scraper.py
+++ starlingbank_niekerk/scraper.py
@@ -11,7 +11,7 @@ This is a temporary script file.
 import sqlite3
 from sqlite3 import Error
 import pandas as pd
-
+import requests
 
 # create a new database - not needed as the connection creates it if needed
 # create connection
@@ -50,4 +50,22 @@ def read_CSV(file):
 def write_to_db(df, table, conn):
     df.to_sql(table, conn, if_exists="replace")
     
-    
+def retrieve_data(sql, conn):
+    df_data = pd.read_sql_query(sql, conn)
+    return df_data
+
+def scrape(url):
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.90 Safari/537.36'}
+    res_1st = requests.get('http://' + url, headers=headers).text
+    return res_1st
+
+def write_to_db_scrape1(company, url, text, links_1st, id_link, conn):
+    sql = """INSERT INTO firstScrape VALUES
+            (
+            ?,?,?,?,?
+            )"""           
+    list_values = [company, url, text, links_1st, id_link]
+    print(list_values)        
+    cur = conn.cursor()
+    cur.execute(sql, list_values)
+    conn.commit()
